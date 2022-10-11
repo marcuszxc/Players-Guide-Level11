6a76a742418456d7968e0c9a21466e5eece28bc6
marcu <marcu@MSI>
2022-10-11 11:14:13 +0200

    Add project files.

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TheMagicCannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{i}: Electric and Fire");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{i}: Electric");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{i}: Fire");  
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{i}: Normal");
+                }
+            }
+        }
+    }
+}
---
+++ ThePrototype/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ThePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The pilot, enters a number between 0 and 100: ");
+            int Pilot = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Hunter, guess the number.");
+
+            int hunter = 0;
+
+            while (true)
+            {
+                Console.Write("What is your next guess? ");
+                hunter = int.Parse(Console.ReadLine());
+
+                if (hunter < Pilot)
+                {
+                    Console.WriteLine($"{hunter} is too low.");
+                }
+                else if (hunter > Pilot) 
+                {
+                    Console.WriteLine($"{hunter} is too high.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed the number!");
+                    break;
+                }
+            }
+        }
+    }
+}
